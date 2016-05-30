b1fb0c18c3f9b381c31d3119215676d50286db60
Soon Yong Chang <sychang1@gmail.com>
2016-05-30 14:13:11 +0900

    Updated

--- plot0.R
+++ plot0.R
@@ -3,7 +3,7 @@
 #
 
 # Run this script just once.
-# Reads in the raw data and formats the "Date" and "Time" into a single variable "date_time".
+# Reads in the raw data.
 # Takes in only the subset of data correspoinding to Feb-1st-2007 and Feb-2nd-2007.
 
 dat <- read.table("household_power_consumption.txt",sep=";",header=TRUE)
